59775151795a36bc811fe16b8e55b12f3d1b90c6
josugoar <josugoar@gmail.com>
2023-10-30 16:34:45 +0100

    Add models and initial migration

--- offers/models.py
+++ offers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Offer(models.Model):
+    categories = models.ManyToManyField(Category)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
