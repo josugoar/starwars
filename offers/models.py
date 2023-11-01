140feb24f8a1444dc81e81eaf08ae724a0abcb55
josugoar <josugoar@gmail.com>
2023-11-01 12:31:01 +0100

    Convert to str

--- offers/models.py
+++ offers/models.py
@@ -2,23 +2,24 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Offer(models.Model):
-    categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category, blank=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
