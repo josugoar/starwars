baea7a25664be81711335b782bceeecb72be7537
josugoar <josugoar@gmail.com>
2023-12-12 14:05:57 +0100

    Refactor queryset in IndexView to return only one featured offer per country

--- offers/views.py
+++ offers/views.py
@@ -6,7 +6,20 @@ from offers.models import Category, Country, Offer
 class IndexView(generic.ListView):
     context_object_name = "featured_offer_list"
     template_name = "offers/index.html"
-    queryset = Offer.objects.filter(featured=True)
+
+    def get_queryset(self):
+        return Offer.objects.raw(
+            """
+            SELECT *
+            FROM (
+                SELECT *,
+                    ROW_NUMBER() OVER (PARTITION BY country_id ORDER BY id) AS rn
+                FROM offers_offer
+                WHERE featured = TRUE
+            ) AS subquery
+            WHERE rn = 1
+            """
+        )
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
